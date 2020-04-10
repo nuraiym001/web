f0caa97474f97a690505250b52414443f096bc3b
Nuraiym-Netullina <62312688+Nuraiym-Netullina@users.noreply.github.com>
2020-04-10 11:29:03 +0600

    Update category-detail.component.spec.ts

--- group-project/src/app/category-detail/category-detail.component.spec.ts
+++ group-project/src/app/category-detail/category-detail.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
-import { CategoryDetailComponent } from './category-detail.component';
+import { Product } from "../product";
+import { PRODUCTS } from "../all-products";
+import { CategoriesService } from "../categories.service";
 
-describe('CategoryDetailComponent', () => {
-  let component: CategoryDetailComponent;
-  let fixture: ComponentFixture<CategoryDetailComponent>;
+import { Category } from '../category';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CategoryDetailComponent ]
-    })
-    .compileComponents();
-  }));
+@Component({
+  selector: 'app-category-detail',
+  templateUrl: './category-detail.component.html',
+  styleUrls: ['./category-detail.component.css']
+})
+export class CategoryDetailComponent implements OnInit {
+  category:Category;
+  products:Product[];
+  product: Product;
+  constructor(
+    private route: ActivatedRoute,
+    private categoriesService: CategoriesService,
+    private location: Location
+  ) { }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CategoryDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  getCategory(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.categoriesService.getCategory(id)
+      .subscribe(category => this.category = category);
+  }
+  getProducts(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.categoriesService.getProductsByCategoryIdFromAllProducts(id)
+      .subscribe(products => this.products = products)
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  add(name: string, id: number, description: string, price: number, img: string, url: string): void {
+    name = name.trim();
+    if (!name||!id||!description||!price||!img||!url) { return; }
+    this.categoriesService.addProduct({ name, id, description, price, img, url } as Product)
+      .subscribe(product => {
+        this.products.push(product);
+      });
+  }
+  delete(product: Product): void {
+    this.products = this.products.filter(p => p !== product);
+    this.categoriesService.deleteProduct(product).subscribe();
+  }
+}
